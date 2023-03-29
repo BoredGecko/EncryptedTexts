d017c886e551faf2c0b48e63c641d811e5c60f48
EvilVolfy <happyhacker872@icloud.com>
2023-03-29 09:36:04 +0300

    Add files via upload

---
+++ EncBotExperimental/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using System.Linq;
+using System.Text;
+
+namespace EncBotExperimental
+{
+    class Program
+    {
+        static ITelegramBotClient bot = new TelegramBotClient("5724375233:AAEmz0ybuyEx4-SLVh04JijFu6gkfUS9b9c");
+        public static List<string> messages = new List<string>();
+        public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            // Некоторые действия
+            Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(update));
+            if (update.Type == Telegram.Bot.Types.Enums.UpdateType.Message)
+            {
+                var message = update.Message;
+                switch (message.Text.ToLower())
+                {
+                    case "/start":
+                        await botClient.SendTextMessageAsync(message.Chat, "Got it.");
+                        messages.Add(message.Text);
+                        return;
+                    case "/encrypt":                         
+                        await botClient.SendTextMessageAsync(message.Chat, "Введите текст для шифрования");
+                        return;
+                    default:
+                        await botClient.SendTextMessageAsync(message.Chat, "Привет-привет!!");
+                        return;
+                }                            
+            }
+        }
+
+        public static async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            // Некоторые действия
+            Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(exception));
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Запущен бот " + bot.GetMeAsync().Result.FirstName);
+
+            var cts = new CancellationTokenSource();
+            var cancellationToken = cts.Token;
+            var receiverOptions = new ReceiverOptions
+            {
+                AllowedUpdates = { }, // receive all update types
+            };
+            bot.StartReceiving(
+                HandleUpdateAsync,
+                HandleErrorAsync,
+                receiverOptions,
+                cancellationToken
+            );
+            Console.ReadKey();
+        }
+    }
+}
